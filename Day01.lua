6d20a3abe101b62fa14a693fbf4f6255e6628043
Matthew Gardian <matt@finesketch.ca>
2020-11-30 22:45:45 -0700

    Add day 1 solution

--- Day01.lua
+++ Day01.lua
@@ -1,4 +1,36 @@
-print("Solving day 1, " .. _VERSION)
+print("Solving day 1")
+
+local numbers = {}
 for line in io.lines("Day01.txt") do
-    print(line)
+    numbers[#numbers + 1] = line + 0
 end
+
+local function findSum2(numbers, sum)
+    for i = 1, #numbers - 1, 1 do
+        for j = i + 1, #numbers, 1 do
+            if (numbers[i] + numbers[j] == sum) then
+                return numbers[i], numbers[j];
+            end
+        end
+    end
+end
+
+local x, y = findSum2(numbers, 2020)
+print(string.format("For two numbers, found %d and %d, Result is %d", x, y, x * y))
+
+--------
+
+local function findSum3(numbers, sum)
+    for i = 1, #numbers - 2, 1 do
+        for j = i + 1, #numbers - 1, 1 do
+            for k = i + 2, #numbers, 1 do
+                if (numbers[i] + numbers[j] + numbers[k] == sum) then
+                    return numbers[i], numbers[j], numbers[k];
+                end
+            end
+        end
+    end
+end
+
+local a, b, c = findSum3(numbers, 2020)
+print(string.format("For three numbers, found %d and %d and %d, Result is %d", a, b, c, a * b * c))
