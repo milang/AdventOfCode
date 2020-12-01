3a25bbf0c78ef412a07409f68787f29e0e1b6ec5
Milan Gardian <mgardian@gmail.com>
2020-12-01 09:13:21 -0700

    Small fixes/polish for day 1

--- Day01.lua
+++ Day01.lua
@@ -1,8 +1,8 @@
 print("Solving day 1")
 
-local numbers = {}
+local parsedNumbers = {}
 for line in io.lines("Day01.txt") do
-    numbers[#numbers + 1] = line + 0
+    parsedNumbers[#parsedNumbers + 1] = tonumber(line)
 end
 
 local function findSum2(numbers, sum)
@@ -15,7 +15,7 @@ local function findSum2(numbers, sum)
     end
 end
 
-local x, y = findSum2(numbers, 2020)
+local x, y = findSum2(parsedNumbers, 2020)
 print(string.format("For two numbers, found %d and %d, Result is %d", x, y, x * y))
 
 --------
@@ -23,7 +23,7 @@ print(string.format("For two numbers, found %d and %d, Result is %d", x, y, x * 
 local function findSum3(numbers, sum)
     for i = 1, #numbers - 2, 1 do
         for j = i + 1, #numbers - 1, 1 do
-            for k = i + 2, #numbers, 1 do
+            for k = j + 1, #numbers, 1 do
                 if (numbers[i] + numbers[j] + numbers[k] == sum) then
                     return numbers[i], numbers[j], numbers[k];
                 end
@@ -32,5 +32,5 @@ local function findSum3(numbers, sum)
     end
 end
 
-local a, b, c = findSum3(numbers, 2020)
+local a, b, c = findSum3(parsedNumbers, 2020)
 print(string.format("For three numbers, found %d and %d and %d, Result is %d", a, b, c, a * b * c))
