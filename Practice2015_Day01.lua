8181c2dcf178d092c21de0d05e110504d5591655
Matthew Gardian <matt@finesketch.ca>
2020-12-01 22:35:37 -0700

    Add practice for 2015, day 1

---
+++ Practice2015_Day01.lua
@@ -0,0 +1,27 @@
+print("Solving 2015, day 1")
+
+local directions = nil
+for line in io.lines("Practice2015_Day01.txt") do
+    directions = line
+end
+
+local floor = 0
+local basement = 0
+for i = 1, #directions do
+    -- print(string.byte(directions,i))
+    -- open: 40
+    -- closed: 41
+    local isOpenParenthesis = string.byte(directions,i) == 40
+    if isOpenParenthesis then
+        floor = floor + 1
+    else
+        floor = floor - 1
+    end
+
+    if floor == -1 and basement == 0 then
+        basement = i
+    end
+end
+
+print(floor)
+print(basement)
