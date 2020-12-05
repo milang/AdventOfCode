8f1dbff8fcf01fd759d55f61fdf92a203c03b15d
Milan Gardian <mgardian@gmail.com>
2020-12-04 21:57:38 -0700

    Parse valid records in day 4

--- Day04.lua
+++ Day04.lua
@@ -26,30 +26,42 @@ print(string.format("Found %d valid records", valid))
 
 ------
 
-local function isYear(value, min, max)
-    if string.match(value, "^%d%d%d%d$") then
+local function parseYear(value, min, max)
+    if value and string.match(value, "^%d%d%d%d$") then
         local year = tonumber(value)
-        return year >= min and year <= max
+        if year >= min and year <= max then return year end
     end
-    return false
+    return nil
 end
 
-local function isHeight(value)
-    local num = string.match(value, "^(%d+)cm$")
-    if num then
-        local cm = tonumber(num)
-        return cm >= 150 and cm <= 193
-    else
-        num = string.match(value, "^(%d+)in$")
+local function parseHeight(value)
+    if value then
+        local num = string.match(value, "^(%d+)cm$")
         if num then
-            local inches = tonumber(num)
-            return inches >= 59 and inches <= 76
+            local cm = tonumber(num)
+            if cm >= 150 and cm <= 193 then return { value = cm, isMetric = true } end
+        else
+            num = string.match(value, "^(%d+)in$")
+            if num then
+                local inches = tonumber(num)
+                if inches >= 59 and inches <= 76 then return { value = inches, isMetric = false } end
+            end
         end
     end
+    return nil
 end
 
-local function isHex(value)
-    return string.match(value, "^#[a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9]$")
+local function parseHex(value)
+    if value and string.match(value, "^#[a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9]$") then
+        return {
+            value = value,
+            number = tonumber(string.sub(value, 2), 16),
+            red = tonumber(string.sub(value, 2, 3), 16),
+            green = tonumber(string.sub(value, 4, 5), 16),
+            blue = tonumber(string.sub(value, 6, 7), 16)
+        }
+    end
+    return nil
 end
 
 local function isEyeColor(value)
@@ -57,23 +69,29 @@ local function isEyeColor(value)
 end
 
 local function isPassportNumber(value)
-    return string.match(value, "^%d%d%d%d%d%d%d%d%d$")
+    return value and string.match(value, "^%d%d%d%d%d%d%d%d%d$")
 end
 
 local function isMoreValid(record)
-    return isValid(record)
-        and isYear(record.byr, 1920, 2002)
-        and isYear(record.iyr, 2010, 2020)
-        and isYear(record.eyr, 2020, 2030)
-        and isHeight(record.hgt)
-        and isHex(record.hcl)
-        and isEyeColor(record.ecl)
-        and isPassportNumber(record.pid)
+    local byr = parseYear(record.byr, 1920, 2002)
+    local iyr = parseYear(record.iyr, 2010, 2020)
+    local eyr = parseYear(record.eyr, 2020, 2030)
+    local hgt = parseHeight(record.hgt)
+    local hcl = parseHex(record.hcl)
+    if byr and iyr and eyr and hgt and hcl and isEyeColor(record.ecl) and isPassportNumber(record.pid) then
+        return { byr = byr, iyr = iyr, eyr = eyr, hgt = hgt, hcl = hcl, ecl = record.ecl, pid = record.pid }
+    end
+    return nil
 end
 
 local moreValid = 0
+local found = {}
 for _, record in ipairs(records) do
-    if isMoreValid(record) then moreValid = moreValid + 1 end
+    local r = isMoreValid(record)
+    if r then
+        table.insert(found, r)
+        moreValid = moreValid + 1
+    end
 end
 
 print(string.format("Found %d more valid records", moreValid))
